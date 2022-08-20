f845af879c2ab4af31109ffc2ed37423a43d5a6d
Vika <vigrinshpum@gmail.com>
2022-08-20 22:19:09 +0300

    Example010

---
+++ example010/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("input number");
+string numberStr;
+numberStr = Console.ReadLine();
+int n = int.Parse(numberStr);
+int a = n* (-1);
+while (a<=n)
+{
+    Console.WriteLine (a);
+    a=a+1;
+    }
