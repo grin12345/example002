9ead2d9eaaea695b72a3dfdeffc9a90ea6f0fe84
Vika <vigrinshpum@gmail.com>
2022-08-20 18:49:17 +0300

    Example009

---
+++ example009/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine ("введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (b*b == a)
+{
+    Console.WriteLine ("Является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine ("Первое число не является квадратом второго числа");
+}
